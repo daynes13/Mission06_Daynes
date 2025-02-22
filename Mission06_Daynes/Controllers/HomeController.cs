6dae0f27ac041c7593c39e34a484160e8547db6a
daynes13 <daynes13@SOPCS-1032206>
2025-02-21 17:16:44 -0700

    Mission 7

--- Mission06_Daynes/Controllers/HomeController.cs
+++ Mission06_Daynes/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission06_Daynes.Models;
 
 namespace Mission06_Daynes.Controllers
@@ -25,16 +26,72 @@ namespace Mission06_Daynes.Controllers
         [HttpGet]
         public IActionResult AddMovie()
         {
-            return View();
+            ViewBag.categories = _context.Categories.ToList();
+            return View("AddMovie", new Movie());
         }
 
         [HttpPost]
-        public IActionResult AddMovie(AddMovie response)
+        public IActionResult AddMovie(Movie response)
         {
-            _context.Movies.Add(response);
+            if (ModelState.IsValid)
+            {
+                _context.Movies.Add(response);
+                _context.SaveChanges();
+
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.categories = _context.Categories.ToList();
+                return View(response);
+            }
+            
+        }
+
+        public IActionResult ViewMovies()
+        {
+            var movies = _context.Movies
+                .Include( x => x.Category)
+                .OrderBy(x => x.Title).ToList();
+
+            return View(movies);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Movies
+                .Single(x => x.MovieId == id);
+            ViewBag.categories = _context.Categories.ToList();
+
+            return View("AddMovie", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie updatedMovie)
+        {
+            _context.Update(updatedMovie);
             _context.SaveChanges();
 
-            return View("Index");
+            return RedirectToAction("ViewMovies");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie movie)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction("ViewMovies");
         }
     }
 }
---
+++ Mission06_Daynes/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission06_Daynes.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
